3be73883b8e98851eb104d34d5f08a5e68c97a05
Paulo Sergio <pauloserg861@gmail.com>
2022-09-26 12:48:40 -0300

    implementando modal de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-login">
-    <div class="size-box">
+    <div class="size-box size-info">
       <div>
         <img src="/img/TallosFlix-removebg-preview.png" alt="Logo TalloFlix">
       </div>
@@ -11,11 +11,22 @@
       <div>
         <button 
           class="btn-login"
-          @click="showLoginBtn"
+          @click="showLoginDisplay"
           v-if="showLoginBtn"
         >
           Faça Login
         </button>
+      </div>
+      <div 
+        class="login-area"
+        v-if="displayModalForm"
+      >
+        <div class="login" @click="toggleUser">
+          (i) Área do usuário
+        </div>
+        <div class="login" @click="toggleAdmin">
+          (i) Área do Admin
+        </div>
       </div>
     </div>
     <div class="size-box">
@@ -28,8 +39,16 @@ export default {
   name: 'Login',
   data() {
     return {
-      showLoginBtn: true
+      showLoginBtn: true,
+      displayModalForm: false,
     }
+  },
+
+  methods: {
+    showLoginDisplay() {
+      this.displayModalForm = true;
+      this.showLoginBtn = false;
+    },
   }
 }
 </script>
@@ -44,4 +63,70 @@ export default {
   display: flex;
   padding: 50px;
 }
+
+.size-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.size-box {
+  width: 50%;
+}
+
+.size-info h1 {
+  font-size: 45px;
+  margin-bottom: 30px;
+}
+
+.size-info img {
+  filter: drop-shadow(0 0 1px #fff);
+  width: 300px;
+}
+
+.size-info .btn-login {
+  padding: 14px 100px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  background-color: rgb(255, 234, 0);
+  transition: .4s;
+  margin: 40px 0;
+}
+
+.size-info .btn-login:hover {
+  background-color: rgb(247, 189, 0);
+}
+
+.size-info .login-area {
+  display: flex;
+  margin: 30px 0;
+  background-color: #009acc;
+  border-radius: 10px;
+  cursor: pointer;
+  animation: roar .4s ease-in;
+}
+
+@keyframes roar {
+  0% {
+    opacity: 0;
+    visibility: hidden;
+  }
+  100% {
+    opacity: 1;
+    visibility: visible;
+  }
+}
+
+.login-area .login {
+  padding: 50px 30px;
+  transition: .5s ease;
+}
+
+.login-area .login:hover {
+  background-color: #00789f;
+  border-radius: 10px;
+}
+
 </style>
