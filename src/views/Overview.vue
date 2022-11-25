42b3f4339ae5245ff31b9d16ca2513c3c9ca1e98
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 17:11:52 -0300

    mockando dados na estrutura

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    Dashboard
-  </div>
+  <main>
+		<div class="card-dash-container">
+			<DashInfo />
+			<DashInfo />
+			<DashInfo />
+			<DashInfo />
+		</div>
+  </main>
 </template>
 
 <script>
+import DashInfo from '../components/DashInfo/DashInfo.vue'
+
 export default {
-  name: 'Overview'
+  name: 'Overview',
+	components: { DashInfo }
 }
 </script>
