ca4b5e6b649cc8d6238d7e668f9b5790a863de6d
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 17:12:26 -0300

    criando componente de card

---
+++ src/components/DashInfo/DashInfo.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="card-dash">
+		<div class="title-infos">
+			<div class="card-icon">
+				<font-awesome-icon :icon="['fas', 'users']" />
+			</div>
+			<div class="card-dash-data">
+				<h6>Usuários ativos na plataforma</h6>
+				<span>250</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'DashInfo',
+}
+</script>
