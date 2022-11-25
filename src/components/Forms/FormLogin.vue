e8393c870dc695073a85e94f1a91d94da7392e38
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 13:46:04 -0300

    criando componente de formulario

---
+++ src/components/Forms/FormLogin.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="form-container">
+		<form @submit.prevent="$emit('submitEvent', user)">
+			<div class="form-title">
+				<Typography :typography_text="'Gerenciador TallosFlix'"/>
+			</div>
+			<div class="data-container">
+				<div>
+					<label for="email">E-mail</label>
+					<div class="input-type">
+						<font-awesome-icon :icon="['fas', 'envelope']" />
+						<BaseInput
+							type="text"
+							id="email"
+							placeholder="email@teste.com"
+							v-model="user.email"
+						/>
+					</div>
+				</div>
+				<div>
+					<label for="passwrod">Senha</label>
+					<div class="input-type">
+						<font-awesome-icon :icon="['fas', 'lock']" />
+						<BaseInput
+							type="password"
+							id="password"
+							placeholder="*******"
+							v-model="user.password"
+						/>
+						<!--<div class="input-styles">
+							<input type="text" placeholder="teste">
+							<font-awesome-icon :icon="['fas', 'eye']"/>
+						</div>-->
+					</div>
+				</div>
+			</div>
+			<div>
+				<p class="sub-text">Plataforma de gerenciamento de recursos Tallosflix</p>
+			</div>
+			<MainButton
+				class="btn-login"
+				type="submit"
+				:title_btn="'Login'"
+			/>
+		</form>
+	</div>
+</template>
+
+<script>
+import BaseInput from "../../assets/sass/components/Inputs/BaseInput.vue";
+import MainButton from "../Buttons/MainButton.vue";
+import Typography from "../Typography/Typography.vue";
+
+export default {
+	name: "FormLogin",
+	components: { MainButton, Typography, BaseInput },
+	emits: ['submitEvent'],
+	data() {
+		return {
+			user: {
+				email: '',
+				password: '',
+			},
+		};
+	},
+};
+</script>
