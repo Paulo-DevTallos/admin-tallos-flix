d9d074571026839a349626bf08a9133e0179cce5
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 13:46:56 -0300

    criando e importando estilos

---
+++ src/assets/sass/components/Inputs/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<input
+		v-bind="$attrs"
+		v-model="inputValue"
+		@blur="handleValue"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'BaseInput',
+	emit: ['handleValue'],
+	props: {
+		currentVal: String,
+	},
+  data() {
+    return {
+			inputValue: ''
+		}
+  },
+  methods: {
+    handleValue(newValue) {
+      this.$emit('handleValue', newValue)
+    }
+  },
+  watch: {
+    currentVal(value) {
+      this.inputValue = value
+    },
+  },
+};
+</script>
