a4cef838f0f667675854fecf062198632e95d735
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 14:57:36 -0300

    criando arquivos de rotas

---
+++ src/contants/router-links.js
@@ -0,0 +1,14 @@
+export default [
+	{
+		id: 0,
+		router: '/admin/users',
+		icon: 'users',
+		description: 'Usuários',
+	},
+	{
+		id: 1,
+		router: '/admin/movies',
+		icon: 'video',
+		description: 'Filmes',
+	},
+]
