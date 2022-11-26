72884876b9817b978d8f9b7e7210a233cac7dbbf
Paulo Sergio <pauloserg861@gmail.com>
2022-11-25 22:19:31 -0300

    criando footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+	<footer class="footer">
+		Equipe de desenvolvimento trainee Tallos io &copy; {{ year }}
+	</footer>
+</template>
+
+<script>
+export default {
+	name: 'Footer',
+	data() {
+		return {
+			year: '',
+		}
+	},
+
+	mounted() {
+		const currentDate = new Date()
+		this.year = currentDate.getFullYear().toLocaleString()
+	}
+}
+</script>
