fac1fc40946f4f51efba56e09145802b3b05d840
Paulo Sergio <pauloserg861@gmail.com>
2022-12-29 15:22:47 -0300

    implementando user profile

--- src/components/Headers/Header.vue
+++ src/components/Headers/Header.vue
@@ -17,14 +17,34 @@
 				<strong>{{ $store.state.userStore.routerName }}</strong>
 			</div>
 		</div>
-		<div class="avatar-container">
+		<div class="avatar-container" @click="callCardProfile">
 			<span> P </span>
 		</div>
+		<CardProfile
+			@close="closeCardProfile"
+			:class="{ isCardProfileActive: isActive }"
+		/>
 	</header>
 </template>
 
 <script>
+import CardProfile from "../Cards/CardProfile.vue";
+
 export default {
 	name: "Header",
+	components: { CardProfile },
+	data() {
+		return {
+			isActive: false,
+		};
+	},
+	methods: {
+		callCardProfile() {
+			this.isActive = true;
+		},
+		closeCardProfile() {
+			this.isActive = false;
+		},
+	},
 };
 </script>
